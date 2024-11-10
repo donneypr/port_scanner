469ea0466b330b872d07a303d58b56c480138d54
donneypr <donatoprabahar@gmail.com>
2024-11-10 00:15:10 -0500

    added user input and cleaner prnt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 
 ports = [21, 22, 80, 443, 23, 25, 53, 110, 143, 445, 3389, 3306, 5432, 8080]
-ip = '192.168.1.1'
+ip = input("Enter the IP address you want to scan: ")
 
 def scan_ports(ip, ports):
     open_ports = []
@@ -15,4 +15,10 @@ def scan_ports(ip, ports):
         sock.close()
     return open_ports 
 
-print(f"Open ports on the specified ip {ip}: {scan_ports(ip, ports)}")
+open_ports = scan_ports(ip,ports)
+
+if open_ports:
+    print(f"Open ports on the specified IP {ip}: ", end="")
+    print("\033[91m" + ", ".join(map(str, open_ports)) + "\033[0m")
+else:
+    print(f"No open ports found on the specified IP {ip}.")

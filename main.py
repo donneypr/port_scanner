3ddcdfb2d7894f373030824f7009f80651d45022
donneypr <donatoprabahar@gmail.com>
2024-11-10 00:05:44 -0500

    working?

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ ip = '192.168.1.1'
 def scan_ports(ip, ports):
     open_ports = []
     for port in ports:
-        socket.socket(socket.AF_INET, socket.SOCK_stream)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
-        result = socket.connect_ex((ip,port))
-        #bruh if the port not open it should return 0
+        #bruh  0 means the port is closed
+        result = sock.connect_ex((ip, port))  
         if result == 0:
             open_ports.append(port)
         sock.close()
-        return open_ports
-    
-    
+    return open_ports 
 
-
-
-
+print(f"Open ports on the specified ip {ip}: {scan_ports(ip, ports)}")
